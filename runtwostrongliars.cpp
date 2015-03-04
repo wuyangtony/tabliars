15b2d93842c56f146df2363639620121c7a72b98
Andrew Shallue <ashallue@iwu.edu>
2015-03-04 09:15:28 -0600

    everything looks good, goal accomplished.  Runtwostrongliars.cpp now has testing code, and all three methods for counting composites with two strong liars match answers.

--- runtwostrongliars.cpp
+++ runtwostrongliars.cpp
@@ -25,18 +25,47 @@ int main(){
 
 // BOUNDPOW is the power of 10 that forms the upper bound of all calculations. 
 // bound is the resulting integer value
-long BOUNDPOW = 5;
+long BOUNDPOW = 4;
 
-ZZ bound = power_ZZ(10, BOUNDPOW);
+long bound = power_long(10, BOUNDPOW);
+time_t start, end; // for timings
 
 // FactoredSieve returns array where nth entry contains smallest prime factor of n
-vector<long> sieve = FactoredSieve(1000);
+time(&start);
+vector<long> sieve = FactoredSieve(bound);
+time(&end);
+cout << "time taken creating factor sieve is " << difftime(end, start) << "\n";
 
 long ncount=0;  // stores the count of odd composites with two strong liars
 
-cout << "bound is " << bound << " and ncount is " << ncount << "\n";
+time(&start);
+// count the number of odd composites with two strong liars using definition
+for(long i = 3; i <= bound; i = i+2){
+  if(sieve.at(i) != i && StrongLiarCount(to_ZZ(i)) == to_ZZ(2)) ncount++;
+}
+time(&end);
 
-cout << sieveTwoStrongLiarsCount(20) << "\n";
+cout << "bound is " << bound << " and ncount is " << ncount;
+cout << " time taken is " << difftime(end, start) << "\n";
+
+time(&start);
+// count the number of composites with two strong liars using Monier
+ncount = 0;
+for(long i = 3; i <= bound; i=i+2){
+  if(sieve.at(i) != i && trialStrongLiarCount(i) == 2) ncount++;
+}
+time(&end);
+
+cout << "bound is " << bound << " and ncount is " << ncount ;
+cout << " time taken is " << difftime(end, start) << "\n";
+
+time(&start);
+// finally, count the number of composites with two strong liars using Shallue sieve
+ncount = sieveTwoStrongLiarsCount(bound);
+time(&end);
+
+cout << "bound is " << bound << " and ncount is " << ncount;
+cout << " time taken is " << difftime(end, start) << "\n";
 
 /*
 for(long i = 3; i < 100; i = i+2){
