e42c4bbdb4624e04478b5dc9a3a15299ab8d8e63
Tony Liu <wliu@iwu.edu>
2017-06-21 10:40:05 -0500

    add the new Odometer class

---
+++ include/Odometer.h
@@ -0,0 +1,43 @@
+// This is a customized class for manipulating relative data structure
+// Author: Tony Liu
+// Date: Jun, 2017
+#ifndef ODOMETER_H
+#define ODOMETER_H
+#include <vector>
+#include <ostream>
+using namespace std;
+
+class Odometer {
+
+	// Properties
+private:
+	long number_of_wheels; // number of digits in odometer object
+	long spins_per_cycle; // number of spins from all zeros to all zeros
+	vector<long> bases; // base for each digit, its size equal to size
+	vector<long> digits; // current digits of odometer
+
+	// Methods
+public:
+	// Constructor
+	Odometer(const vector<long>& b);
+
+	// Destructor
+	~Odometer();
+
+	// return the size of the odometer
+	long size() const;
+
+	// get specific digit of the odometer
+	long get(const long& n) const;
+
+	// spin the wheels n times, default n = 1
+	void spin(long n=1);
+
+	// overload operator = as assignment
+	void operator=(const Odometer& o);
+
+	// overload operator << to display current digits
+	friend ostream& operator<<(ostream& out, const Odometer& o);
+};
+
+#endif
---
+++ src/Odometer.cpp
@@ -0,0 +1,67 @@
+// Implementation of Odometer class
+#include "../include/Odometer.h"
+
+// Constructor
+//   parameter b is the vector of bases to initialize the odometer
+Odometer :: Odometer(const vector<long>& b) {
+	// initialize the bases
+	bases = b;
+	// get the number of wheels/digits
+	number_of_wheels = b.size();
+	// calculate spins per cycle, which is equal to
+	// multiplication of all bases
+	spins_per_cycle = b.at(0);
+	for (long i=1; i<number_of_wheels; i++) {
+		spins_per_cycle *= b.at(i);
+	}
+	// initialize the digits
+	digits.reserve(number_of_wheels);
+	// default digits are zeros
+	for (long i=0; i<number_of_wheels; i++) {
+		digits.push_back(0);
+	}
+}
+
+// Get the number of digits of odometer object
+long Odometer :: size() const {
+	return number_of_wheels;
+}
+
+long Odometer :: get(const long& n) const {
+	return digits.at(n);
+}
+
+// Spin the wheels n times
+void Odometer :: spin(long n) {
+	// keep track of carries on all digits
+	long carry = n % spins_per_cycle;
+	// temprarily store old digit
+	long temp;
+	// add 1 starting from last item of digits and check carries
+	for (long i=number_of_wheels-1; i>=0; i--) {
+		temp = (digits.at(i) + carry) % bases.at(i);
+		carry = (digits.at(i) + carry) / bases.at(i);
+		digits.at(i) = temp;
+	}
+}
+
+void Odometer :: operator=(const Odometer& o) {
+	number_of_wheels = o.number_of_wheels;
+	bases = o.bases;
+	digits = o.digits;
+}
+
+ostream& operator<<(ostream& out, const Odometer& o) {
+	out << "[";
+	for (long i=0; i<o.number_of_wheels-1; i++) {
+		out << o.digits.at(i) << ", ";
+	}
+	out << o.digits.at(o.number_of_wheels-1) << "]";
+	return out;
+}
+
+// Destructor
+Odometer :: ~Odometer() {
+	bases.clear();
+	digits.clear();	
+}
